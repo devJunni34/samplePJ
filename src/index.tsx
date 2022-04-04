5d90dec3d5dd6a14d0a80231ee16643f027301de
junyong.park <junyong.park@onepredict.com>
2022-04-04 13:26:22 +0900

    P2-377 하하하하하하하

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './AppContainer';
 
+console.log(`🚀 11 =================>`, 11);
 ReactDOM.render(
   <React.StrictMode>
     <AppContainer />
